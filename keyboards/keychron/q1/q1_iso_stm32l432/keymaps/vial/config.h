8afba53cc1da130600e7926636075b55ceadc309
kefydra <119268532+kefydra@users.noreply.github.com>
2022-11-28 02:31:53 +0200

    Add files via upload

---
+++ keyboards/keychron/q1/q1_iso_stm32l432/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAD, 0x1C, 0x0F, 0x63, 0x1B, 0xDC, 0x82, 0x03}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+/* Change USB Polling Rate to 1000hz*/
+#define USB_POLLING_INTERVAL_MS 1
